7e92841a818dbdd7fdb3c4f847b8484d9f3b56f6
Jan Mendoza V <u201924912@upc.edu.pe>
2025-05-07 13:45:46 -0500

    Initial commit

---
+++ src/main/java/upc/com/visiontech2/Visiontech2Application.java
@@ -0,0 +1,13 @@
+package upc.com.visiontech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Visiontech2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Visiontech2Application.class, args);
+    }
+
+}
