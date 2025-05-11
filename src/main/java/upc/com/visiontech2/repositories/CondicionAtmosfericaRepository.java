e69eaca76a5805538f10556c691b2b06afa9bd66
JaMeVi <u201924912@upc.edu.pe>
2025-05-11 18:48:42 -0500

    Update CondicionAtmosfericaRepository.java
    CondicionAtmosfericaRepository

--- src/main/java/upc/com/visiontech2/repositories/CondicionAtmosfericaRepository.java
+++ src/main/java/upc/com/visiontech2/repositories/CondicionAtmosfericaRepository.java
@@ -6,4 +6,12 @@ import upc.com.visiontech2.entities.CondicionAtmosferica;
 
 @Repository
 public interface CondicionAtmosfericaRepository extends JpaRepository<CondicionAtmosferica, Integer> {
-}
+
+    @Query("SELECT ca FROM CondicionAtmosferica ca " +
+            "WHERE LOWER(TRIM(ca.ruta.nombreRuta)) = LOWER(TRIM(:nombreRuta)) " +
+            "AND ca.fechaHora BETWEEN :fechaInicio AND :fechaFin")
+    List<CondicionAtmosferica> findByNombreRutaAndFechaHoraBetween(
+            @Param("nombreRuta") String nombreRuta,
+            @Param("fechaInicio") LocalDateTime fechaInicio,
+            @Param("fechaFin") LocalDateTime fechaFin
+    );
