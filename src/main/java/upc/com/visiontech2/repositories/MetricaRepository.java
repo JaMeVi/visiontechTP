6d2649a71a8237583de187040711b19bc83e7610
JaMeVi <u201924912@upc.edu.pe>
2025-05-11 18:50:56 -0500

    Update MetricaRepository.java
    MetricaRepository

--- src/main/java/upc/com/visiontech2/repositories/MetricaRepository.java
+++ src/main/java/upc/com/visiontech2/repositories/MetricaRepository.java
@@ -6,4 +6,25 @@ import upc.com.visiontech2.entities.Metrica;
 
 @Repository
 public interface MetricaRepository extends JpaRepository<Metrica,Integer> {
+
+    @Query("SELECT m FROM Metrica m JOIN m.ruta r WHERE LOWER(r.nombreRuta) = LOWER(:nombreRuta)")
+    List<Metrica> findByNombreRutaContaining(@Param("nombreRuta") String nombreRuta);
+
+
+    @Query("SELECT COALESCE(SUM(m.caloriasQuemadas), 0) " +
+            "FROM Metrica m " +
+            "WHERE LOWER(TRIM(m.ruta.nombreRuta)) = LOWER(TRIM(:nombreRuta))")
+    int sumCaloriasByNombreRuta(@Param("nombreRuta") String nombreRuta);
+
+
+  /* //SQL
+  @Query(
+            value = """
+    SELECT SUM(m.caloria_quemadas) FROM metricas m
+            JOIN rutas r ON m.id_ruta = r.id_ruta
+    WHERE LOWER(TRIM(r.nombre_ruta)) = LOWER(TRIM(:nombreRuta))
+    """,
+            nativeQuery = true
+    )
+    Integer sumCaloriasByNombreRuta(@Param("nombreRuta") String nombreRuta);*/
 }
